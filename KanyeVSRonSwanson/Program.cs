87d7ee99911836ccab18fd9f3b3dbc782f74009f
Baines Deal <bainesdeal@gmail.com>
2022-07-05 11:18:39 -0500

    Add project files.

---
+++ KanyeVSRonSwanson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KanyeVSRonSwanson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
